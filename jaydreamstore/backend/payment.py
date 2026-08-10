ce2d043724d052f28b3d7ae087313b3872f6d951
JaidenZhang <jaidenzhang123@gmail.com>
2026-08-10 18:57:24 +0700

    Delete jaydreamstore/backend directory

--- jaydreamstore/backend/payment.py
+++
@@ -1,8 +0,0 @@
-import midtransclient
-import config
-
-snap = midtransclient.Snap(
-    is_production=True,
-    server_key=config.MIDTRANS_SERVER_KEY,
-    client_key=config.MIDTRANS_CLIENT_KEY
-)
